5060a6af36752113b0101eedba5426cc85535abd
Petr Solovev <p.v.solovev@tinkoff.ru>
2023-09-22 22:24:41 +0300

    setup project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
+import { TuiRootModule, TuiDialogModule, TuiAlertModule } from "@taiga-ui/core";
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TuiRootModule, TuiDialogModule, TuiAlertModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'soa-ui';
-}
+export class AppComponent {}
+
