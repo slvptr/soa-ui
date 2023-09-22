95852838a0495bb1c7f8a80f3a7e61c288c63dc6
Petr Solovev <p.v.solovev@tinkoff.ru>
2023-09-22 18:21:53 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'soa-ui';
+}
